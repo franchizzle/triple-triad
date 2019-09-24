96c1bdcb3f8b827727c926f696ebdfd3e7f109a1
Francesca Reyes <francheesey@gmail.com>
2019-09-23 22:12:31 -0700

    init triple triad

---
+++ src/App.js
@@ -0,0 +1,267 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
+
+
+class GameBoard extends Component {
+  onCardClick(id) {
+    this.props.moves.selectCard(id);
+  }
+
+  // when grid cell is clicked
+  onClick(id) {
+    console.log(id);
+    if(this.isActive(id)) {
+      this.props.moves.selectCell(id);
+      this.props.events.endTurn();
+    }
+  }
+
+  isActive(id) {
+    if (!this.props.isActive) return false;
+    if (this.props.G.cells[id] !== null) return false;
+    return true;
+  }
+
+  isFirstPlayer() {
+    return this.props.ctx.currentPlayer === '0';
+  }
+
+  playerCaptured(id) {
+    if (this.props.G.cells[id] === null) {
+      return 'blank';
+    } else {
+      // if it's player 1 that put the card, return player1 class
+      // else, if it's player 2,  return player2 class
+    }
+  }
+
+  render() {
+    let winner = '';
+    if (this.props.ctx.gameover) {
+      winner = this.props.ctx.gameoever.winner !== undefined ? (
+        <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
+      ) : (
+        <div id="winner">Draw!</div>
+      );
+    }
+    
+    return (
+      <div className="tripleTriadGame">
+        <div className={`player player1 ${this.isFirstPlayer() ? "current" : ""}`}>
+          {
+              this.props.G.firstPlayerHand.map((card, index) => {
+                return (
+                  <div className="card" onClick={() => this.onCardClick(index)}>
+                    { card.map((v) => {
+                        return (
+                          <div className="cardScore">{v}</div>
+                        )
+                      })
+                    }
+                  </div>
+                )
+              })
+            }
+        </div>
+        <div className="board">
+          {
+            this.props.G.cells.map((id, index) => {
+              let cellValue = this.props.G.selectedCard ? id : "";
+              return (
+                <div key={index} className="cell" onClick={() => this.onClick(index)}>
+                  <div className={`card selected ${this.playerCaptured(index)}`}>
+                    { 
+                      cellValue ? 
+                      (cellValue.map((v) => {
+                        return (
+                          <div className="cardScore">{v}</div>
+                        )
+                      })) :
+                      ""
+                    }
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className={`player player2 ${this.isFirstPlayer() ? "" : "current"}`}>
+          {
+            this.props.G.secondPlayerHand.map((card, index) => {
+              return (
+                <div className="card" onClick={() => this.onCardClick(index)}>
+                  { card.map((v) => {
+                      return (
+                        <div className="cardScore">{v}</div>
+                      )
+                    })
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+        { winner }
+      </div>
+    )
+  }
+}
+
+function isWin() {
+
+}
+
+function isDraw() {
+
+}
+
+function checkCard(direction, playerCard, enemyCard, inv) {
+  let flipCard = false;
+  if (enemyCard) {
+    console.log("Enemy " + enemyCard, "Player " + playerCard, "Direction " + direction);
+    if (playerCard[direction] > enemyCard[inv]) {
+      flipCard = true;
+      console.log(flipCard, "yasss");
+      return flipCard;
+    }
+  }
+  // if yes, check if playerCard > enemyCard in corresponding direction
+}
+
+function randomizeCards(ctx) {
+  let cards = [];
+  for(let i = 0; i < 5; i++) {
+    cards.push(ctx.random.Die(10, 4));
+    // cards = source.reduce((obj, arrValue) => (obj[arrValue] = ctx.random.Die(10), obj), {}); 
+    // hand.push(Object.entries(cards));
+  }
+  return cards;
+}
+
+function checkNeighbors(id) {
+  switch (id) {
+    case 0:
+      return [1, 3];
+    case 1:
+        return [0, 2, 4];
+    case 2:
+        return [1, 5];
+    case 3:
+        return [0, 4, 6];
+    case 4:
+        return [1, 3, 5, 7];
+    case 5:
+        return [2, 4, 8];
+    case 6:
+        return [3, 7];
+    case 7:
+        return [4, 6, 8];
+    case 8:
+        return [5, 7];
+    default:
+      console.log('Sorry, we are out of ' + id + '.');
+  }
+}
+
+function removeCardFromHand(card, id) {
+  card.splice(id, 1);
+}
+
+const TripleTriad = Game({
+  setup: (ctx) => ({ 
+    cells: Array(9).fill(null),
+    firstPlayerHand: randomizeCards(ctx),
+    secondPlayerHand: randomizeCards(ctx),
+    selectedCard: null,
+    firstPlayerCaptures: [],
+    secondPlayerCaptures: [],
+  }),
+  moves: {
+    // select card, select cell
+    selectCard(G, ctx, id) {
+      if (ctx.currentPlayer === '0') {
+        G.selectedCard = G.firstPlayerHand[id];
+      } else {
+        G.selectedCard = G.secondPlayerHand[id];
+      }
+    },
+
+    selectCell(G, ctx, id) {
+      let player = null;
+      if (G.cells[id] === null) {
+        G.cells[id] = G.selectedCard;
+        if (ctx.currentPlayer === '0') {
+          G.firstPlayerCaptures.push(id);
+          player = G.firstPlayerHand;
+        } else {
+          G.secondPlayerCaptures.push(id);
+          player = G.secondPlayerHand;
+        }
+        removeCardFromHand(player);
+      }
+
+      const neighbors = checkNeighbors(id);
+      neighbors.map((dir) => {
+        if (G.cells[dir] !== null) {
+          let val = null;
+          let inv = null;
+          if (id+1 === dir) {
+            val = 1;
+            inv = 3;
+          } 
+          if (id+3 === dir) {
+            val = 2;
+            inv = 0;
+          }
+          if (id-1 === dir) {
+            val = 3;
+            inv = 1;
+          }
+          if (id-3 === dir) {
+            val = 0;
+            inv = 2;
+          }
+
+          if ((ctx.currentPlayer === '0' && G.secondPlayerCaptures.includes(dir)) || (ctx.currentPlayer === '1' && G.firstPlayerCaptures.includes(dir))) {
+            let capture = checkCard(val, G.selectedCard, G.cells[dir], inv);
+            if (capture) {
+              if (ctx.currentPlayer === '0') {
+                const index = G.secondPlayerCaptures.indexOf(dir);
+                G.secondPlayerCaptures.splice(index, 1);
+                G.firstPlayerCaptures.push(dir);
+              } else {
+                const index = G.firstPlayerCaptures.indexOf(dir);
+                G.firstPlayerCaptures.splice(index, 1);
+                G.secondPlayerCaptures.push(dir);
+              }
+            }
+          };
+        }
+      })
+    }
+  },
+
+  flow: {
+    endGameIf: (G, ctx) => {
+      // if win
+      if (G.cells.every(cell => cell !== null)) {
+        console.log("end game");
+        if (G.secondPlayerCaptures.length > G.firstPlayerCaptures) {
+          
+        } else {
+
+        }
+      }
+      // if draw
+    },
+  }
+});
+
+const App = Client({ 
+  game: TripleTriad,
+  board: GameBoard,
+});
+
+export default App;
---
+++ src/components/GameView.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
+
+class GameView extends Component {
+  TripleTriad = Game({
+    setup: () => ({ cells: Array(9).fill(null)}),
+    moves: {
+      // select card, select cell
+      selectCard(G, ctx, id) {
+
+      },
+
+      selectCell(G, ctx, id) {
+        G.cells[id] = ctx.currentPlayer;
+      }
+    }
+  });
+
+  render() {
+    return (
+      <div>hi
+        {
+          Client( { game: GameView.TripleTriad })
+        }
+      </div>
+    )
+  }
+}
+
+export default GameView;
