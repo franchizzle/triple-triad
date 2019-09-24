1d20dac4fef1cb7b0205fa800cd96ed0d4cca82f
JJ <justinjcode@gmail.com>
2019-09-24 12:34:51 -0700

    added FlipCards and unsetSelectedCard to utilities

--- src/utilities.js
+++ src/utilities.js
@@ -49,6 +49,51 @@ export function checkNeighbors(id) {
   }
 }
 
+export function unsetSelectedCard(G) {
+  G.selectedCard = null;
+}
+
+export function flipCards(G, ctx, id) {
+  const neighbors = checkNeighbors(id);
+  neighbors.map((dir) => {
+    if (G.cells[dir].card !== null) {
+      let val = null;
+      let inv = null;
+      if (id+1 === dir) {
+        val = 1;
+        inv = 3;
+      }
+      if (id+3 === dir) {
+        val = 2;
+        inv = 0;
+      }
+      if (id-1 === dir) {
+        val = 3;
+        inv = 1;
+      }
+      if (id-3 === dir) {
+        val = 0;
+        inv = 2;
+      }
+
+      if ((ctx.currentPlayer === '0' && G.secondPlayerCaptures.includes(dir)) || (ctx.currentPlayer === '1' && G.firstPlayerCaptures.includes(dir))) {
+        let capture = checkCard(val, G.selectedCard, G.cells[dir], inv, ctx.currentPlayer);
+        if (capture) {
+          if (ctx.currentPlayer === '0') {
+            const index = G.secondPlayerCaptures.indexOf(dir);
+            G.secondPlayerCaptures.splice(index, 1);
+            G.firstPlayerCaptures.push(dir);
+          } else {
+            const index = G.firstPlayerCaptures.indexOf(dir);
+            G.firstPlayerCaptures.splice(index, 1);
+            G.secondPlayerCaptures.push(dir);
+          }
+        }
+      }
+    }
+  });
+}
+
 export function isWin() {
 
 }
