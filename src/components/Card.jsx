dd35b4d8a96e8a8674198930945e276c379534fd
Francesca Reyes <francheesey@gmail.com>
2019-09-25 22:16:44 -0700

    add image into image wrapper div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,24 @@ import React, { Component } from 'react';
 class Card extends Component {
   render() {
     const { card, index, player } = this.props;
-    console.log(card);
-    debugger;
     if (!card.values.map) {
       return (
         <div></div>
       )
     } else {
     return (
-      <div className={"card " + player} onClick={() => this.props.onCardClick(index)} style={{backgroundImage: card.image}}>
-        <div className="card-scores">
-          { card.values.map((v) => {
-              return (
-                <p className="card-score">{v}</p>
-              )
-            })
-          }
+      <div className={"card " + player} onClick={() => this.props.onCardClick(index)}>
+        <div className="card-image-wrapper" style={{backgroundImage: card.image}}>
+          <div className="card-scores">
+            { card.values.map((v) => {
+                return (
+                  <p className="card-score">
+                    {v === '10' ? 'A' : v }
+                  </p>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     )
